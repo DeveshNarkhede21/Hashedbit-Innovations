d110314ddf7f8004cdd636155fbf3fa6a2e3fb10
DeveshNarkhede21 <deveshnarkhede4921@gmail.com>
2024-05-21 12:17:47 +0530

    Add files via upload

---
+++ React Assignment 3 - Survey Form/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import SurveyForm from './SurveyForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Survey Form</h1>
+      <SurveyForm />
+    </div>
+  );
+}
+
+export default App;
